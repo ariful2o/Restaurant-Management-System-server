20cb4285ef4686913655de778e23f2925f690ab1
mohammadariful20 <arifulz848@gmail.com>
2024-05-13 01:13:11 +0600

    get products,product and ppost products

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
@@ -14,6 +15,54 @@ app.use(cors({
     ],
     credentials: true,
 }))
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.BD_PASSWORD}@cluster0.0zrlznh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const database = client.db("Restaurant_Management");
+    const  products= database.collection("foods");
+
+    app.get('/products',async(req,res)=>{
+        const result = await products.find().toArray();
+        res.json(result);
+    })
+    app.get('/product/:id',async(req,res)=>{
+        const id = req.params.id;
+        const result = await products.findOne({_id:ObjectId(id)});
+        res.json(result);
+    })
+
+    app.post('/product',async(req,res)=>{
+        const newProduct = req.body;
+        const result = await products.insertOne(newProduct);
+        res.json(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
