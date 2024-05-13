9049316937985159a4ef0be48b90db5811c219df
mohammadariful20 <arifulz848@gmail.com>
2024-05-13 19:44:20 +0600

    addtocard ,add order,get addtocard and get order

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const port = process.env.PORT || 5000
 //midlewere
 app.use(express.json())
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://cardoctor-bd.web.app",
-        "https://cardoctor-bd.firebaseapp.com",
-    ],
-    credentials: true,
+  origin: [
+    "http://localhost:5173",
+    "https://cardoctor-bd.web.app",
+    "https://cardoctor-bd.firebaseapp.com",
+  ],
+  credentials: true,
 }))
 
 
@@ -34,31 +34,61 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
+
+
+
     // Get a handle to the database
     const database = client.db("Restaurant_Management");
-    const  products= database.collection("foods");
+    const products = database.collection("foods");
+    const order = database.collection("order");
+    const addToCard = database.collection("addToCard");
 
-    app.get('/products',async(req,res)=>{
-        const result = await products.find().toArray()
-        res.send(result);
+
+    app.get('/products', async (req, res) => {
+      const result = await products.find().toArray()
+      res.send(result);
     })
-    app.get('/products/:id',async(req,res)=>{
-        const id = req.params.id;
-        console.log(id)
-        const quary={_id: new ObjectId(id)}
-        const result = await products.findOne(quary);
-        res.send(result)
+    app.get('/order/:email', async (req, res) => {
+      const email=req.params.email
+      const quary = { email: email }
+      const result = await order.find(quary).toArray()
+      res.send(result);
+    })
+    app.get('/addtocard/:email', async (req, res) => {
+      const email = req.params.email;
+      const quary = { email: email }
+      const result = await addToCard.find(quary).toArray()
+      res.send(result);
+    })
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const quary = { _id: new ObjectId(id) }
+      const result = await products.findOne(quary);
+      res.send(result)
     })
 
-    app.post('/product',async(req,res)=>{
-        const newProduct = req.body;
-        const result = await products.insertOne(newProduct);
-        res.send(result);
+
+
+    app.post('/product', async (req, res) => {
+      const newProduct = req.body;
+      const result = await products.insertOne(newProduct);
+      res.send(result);
     })
+    app.post('/order', async (req, res) => {
+      const orderDetails = req.body
+      const result = await order.insertOne(orderDetails);
+      res.send(result)
+    })
+    app.post('/addtocard', async (req, res) => {
+      const addToCardDetails = req.body
+      const result = await order.insertOne(addToCard);
+      res.send(result)
+    })
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -69,9 +99,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Resturant Management Server is Running')
+  res.send('Resturant Management Server is Running')
 })
 
 app.listen(port, () => {
-    console.log(`Resturant Management Server listening on port ${port}`)
+  console.log(`Resturant Management Server listening on port ${port}`)
 })
