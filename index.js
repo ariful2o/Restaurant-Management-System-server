70fd50806686335284035efbd6892926075fd47c
mohammadariful20 <arifulz848@gmail.com>
2024-05-14 17:05:47 +0600

    jwt cookes cteate and send client side

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const cors = require('cors')
 const app = express()
@@ -10,12 +11,11 @@ app.use(express.json())
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://cardoctor-bd.web.app",
-    "https://cardoctor-bd.firebaseapp.com",
+    "https://restaurant-management-37bf5.web.app",
+    "https://restaurant-management-37bf5.firebaseapp.com",
   ],
   credentials: true,
 }))
-
 
 
 
@@ -30,6 +30,12 @@ const client = new MongoClient(uri, {
   }
 });
 
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -43,7 +49,29 @@ async function run() {
     const order = database.collection("order");
     const addToCard = database.collection("addToCard");
 
+    //auth api
+    app.post('/jwt', async (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECTET, { expiresIn: '1h' })
 
+
+      res.cookie('token', token, cookieOptions).send({ success: true })
+    })
+
+    // //clearing Token
+    // app.post("/logout", async (req, res) => {
+    //   const user = req.body;
+    //   console.log("logging out", user);
+    //   res
+    //     .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+    //     .send({ success: true });
+    // });
+
+
+
+
+
+    //services api
     app.get('/products', async (req, res) => {
       const result = await products.find().toArray()
       res.send(result);
@@ -91,17 +119,17 @@ async function run() {
     })
 
 
-    app.put('/updatequantete/:id',async(req,res)=>{
+    app.put('/updatequantete/:id', async (req, res) => {
       const id = req.params.id;
-      const updateqty=req.body
+      const updateqty = req.body
       const quary = { _id: new ObjectId(id) }
       const options = { upsert: true };
-      const doc={
+      const doc = {
         $set: {
-        Quantity:updateqty.Quantity
-      },
+          Quantity: updateqty.Quantity
+        },
       }
-      const result = await products.updateOne(quary,doc,options)
+      const result = await products.updateOne(quary, doc, options)
       res.send(result)
     })
 
